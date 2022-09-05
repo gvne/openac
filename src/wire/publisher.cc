d1f01b4284fc2c1420ce7c9efc7d30bc27584054
gvne <guillaume.vincke@gmail.com>
2022-09-05 11:26:34 +0200

    Convert the dntp port to the right endianness in wire message

--- src/wire/publisher.cc
+++ src/wire/publisher.cc
@@ -28,27 +28,28 @@ void Publisher::AddSubscriber(asio::ip::udp::endpoint sub_addr) {
 
 void Publisher::Reset() {
   spdlog::trace("wire - Resetting the publisher");
-  
+
   message_.flags = Message::kDefaultFlags;
   message_.sequence_number = 0;
   message_.timestamp = 0;
   message_.ssrc = 0;
-  
+
   // Extension. Specific data related to oac. Mostly used for sync
   message_.extension_id = Message::kDefaultExtensionID;
   message_.extension_length = mem::ToBigEndian(Message::kExtensionLength);
   message_.extension.version = Message::Extension::kDefaultVersion;
-  message_.extension.ntp_server_port = dntp_server_.endpoint().port();
+  message_.extension.ntp_server_port = \
+    mem::ToBigEndian(dntp_server_.endpoint().port());
   auto ipv4_addr = dntp_server_.endpoint().address().to_v4().to_bytes();
   message_.extension.ntp_server_address = ipv4_addr;
   message_.extension.reference_timestamp = dntp_server_.Now().Pack();
-  
+
   pending_sample_count_ = 0;
 }
 
 void Publisher::Publish(const int16_t* data, std::size_t sample_count) {
   std::size_t index = 0;
-  
+
   // The first message to send should include the remaining data if any
   if (pending_sample_count_ != 0) {
     // Not enough data to send a message
@@ -71,17 +72,17 @@ void Publisher::Publish(const int16_t* data, std::size_t sample_count) {
               message_.data.data() + pending_sample_count_);
     index = missing_sample_count;
     pending_sample_count_ = 0;
-    
+
     Publish();
   }
-  
+
   while (index + message_.data.size() < sample_count) {
     // Fill data
     std::copy(data + index,
               data + index + message_.data.size(),
               message_.data.data());
     index += message_.data.size();
-    
+
     Publish();
   }
   // keep the remaining samples
@@ -94,7 +95,7 @@ void Publisher::Publish(const int16_t* data, std::size_t sample_count) {
 void Publisher::Publish() {
   spdlog::trace("wire - Publishing a new message");
   std::error_code err;
-  
+
   for (const auto& endpoint : receivers_addr_) {
     spdlog::trace("wire - Sending to {}:{}", endpoint.address().to_string(), endpoint.port());
     socket_.send_to(asio::buffer(&message_, sizeof(Message)), endpoint, 0, err);
@@ -103,7 +104,7 @@ void Publisher::Publish() {
       err.clear();
     }
   }
-  
+
   // update message metadata
   message_.sequence_number += 1;
   message_.timestamp += message_.data.size();
